3e5081aee82d95284c2225edc0299a90cd8cdd29
leiggee <leiggee@126.com>
2016-07-23 08:18:51 +0800

    我写好了核心代码0.0.2
    我就是描述

---
+++ DZTASI/DZTASI/DZTASI/ViewController/DZTASIViewController.h
@@ -0,0 +1,13 @@
+//
+//  DZTASIViewController.h
+//  DZTASI
+//
+//  Created by apple on 16/7/23.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZTASIViewController : UIViewController
+- (void)demo;
+@end
---
+++ DZTASI/DZTASI/DZTASI/ViewController/DZTASIViewController.m
@@ -0,0 +1,40 @@
+//
+//  DZTASIViewController.m
+//  DZTASI
+//
+//  Created by apple on 16/7/23.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "DZTASIViewController.h"
+
+@interface DZTASIViewController ()
+
+@end
+
+@implementation DZTASIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (void)demo
+{
+    NSLog(@"我就是擎天柱我要拯救地球");
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
